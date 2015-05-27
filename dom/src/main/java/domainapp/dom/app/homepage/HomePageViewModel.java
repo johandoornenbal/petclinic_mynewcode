9b6a8a33cab4dc1788296c4bedc1c9731be9b13b
Johan Doornenbal <johan@filternet.nl>
2015-05-27 18:06:33 +0200

    building and running

--- dom/src/main/java/domainapp/dom/app/homepage/HomePageViewModel.java
+++ dom/src/main/java/domainapp/dom/app/homepage/HomePageViewModel.java
@@ -19,32 +19,91 @@
 package domainapp.dom.app.homepage;
 
 import java.util.List;
+import java.util.SortedSet;
 
+import javax.inject.Inject;
+
+import com.google.common.base.Predicate;
+
+import org.apache.isis.applib.DomainObjectContainer;
+import org.apache.isis.applib.annotation.CollectionLayout;
+import org.apache.isis.applib.annotation.ParameterLayout;
+import org.apache.isis.applib.annotation.RenderType;
 import org.apache.isis.applib.annotation.ViewModel;
 
 import domainapp.dom.modules.petclinic.Pet;
+import domainapp.dom.modules.petclinic.PetOwner;
+import domainapp.dom.modules.petclinic.PetOwners;
+import domainapp.dom.modules.petclinic.PetSpecies;
 import domainapp.dom.modules.petclinic.Pets;
+import domainapp.dom.modules.petclinic.Visit;
+import domainapp.dom.modules.petclinic.Visits;
 
 @ViewModel
 public class HomePageViewModel {
 
-    //region > title
+    public HomePageViewModel() {
+    }
+
     public String title() {
-        return getObjects().size() + " pets";
+        return "Homepage";
     }
-    //endregion
 
-    //region > object (collection)
-    @org.apache.isis.applib.annotation.HomePage
-    public List<Pet> getObjects() {
-        return pets.listAll();
+    @CollectionLayout(render = RenderType.EAGERLY)
+    public List<Visit> getActiveVisits() {
+        return container.allMatches(Visit.class, (Predicate<Visit>) input -> input.getCheckOutTime() == null);
     }
-    //endregion
 
-    //region > injected services
+    @CollectionLayout(render = RenderType.EAGERLY)
+    public List<Visit> getInactiveVisits() {
+        return container.allMatches(Visit.class, (Predicate<Visit>) input -> input.getCheckOutTime() != null);
+    }
 
-    @javax.inject.Inject
-    Pets pets;
+    public HomePageViewModel visitNewPetAndOwner(
+            final @ParameterLayout(named = "Pet Name") String petName,
+            final @ParameterLayout(named = "Pet Species") PetSpecies species,
+            final @ParameterLayout(named = "Owner First Name") String ownerFirstName,
+            final @ParameterLayout(named = "Owner Last Name") String ownerLastName,
+            final @ParameterLayout(named = "Notes", multiLine = 3) String notes
+    ) {
+        visits.addVisit(pets.create(petName, species, petOwners.addOwner(ownerFirstName, ownerLastName)), notes);
+        return this;
+    }
 
-    //endregion
+    public HomePageViewModel visitForExistingPet(
+            final @ParameterLayout(named = "Pet") Pet pet,
+            final @ParameterLayout(named = "Notes", multiLine = 3) String notes
+    ) {
+        visits.addVisit(pet, notes);
+        return this;
+    }
+
+    public HomePageViewModel visitForExistingOwner(
+            final @ParameterLayout(named = "Owner") PetOwner owner,
+            final @ParameterLayout(named = "Pet") Pet pet,
+            final @ParameterLayout(multiLine = 3) String notes
+    ) {
+        visits.addVisit(pet, notes);
+        return this;
+    }
+
+    public SortedSet<Pet> choices1VisitForExistingOwner(
+            final PetOwner owner,
+            final Pet pet,
+            final String notes
+    ) {
+        return owner == null ? null : owner.getPets();
+    }
+
+    @Inject
+    private Visits visits;
+
+    @Inject
+    private Pets pets;
+
+    @Inject
+    private PetOwners petOwners;
+
+    @Inject
+    private DomainObjectContainer container;
 }
